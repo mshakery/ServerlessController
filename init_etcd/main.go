cae68dcc2c6cac6d624b8cf81d2bdd5fb4392a38
Arshia on Steroids <arshshia@gmail.com>
2024-12-03 23:45:50 +0100

    desert rose LA LA

--- init_etcd/main.go
+++ init_etcd/main.go
@@ -8,26 +8,108 @@ import (
 )
 
 func main() {
-	users := []protos.User{
-		{Token: "xaxaxaxa", Uid: "0", UserName: "masoud"},
-		{Token: "ihihihih", Uid: "1", UserName: "roy"},
-		{Token: "lolololo", Uid: "2", UserName: "arshia"},
-	}
+	addUsers, addRoles, addRoleBinding := false, false, false
 
 	cli, err := etcdMiddleware.ConnectToEtcd()
 	if err != nil {
 		panic(err)
 	}
-	for _, user := range users {
-		ku := fmt.Sprintf("/cluster/resources/user/%s", user.Token)
-		err = etcdMiddleware.WriteToEtcdFromPb(cli, context.Background(), ku, &user)
-		if err != nil {
-			fmt.Printf("ik kan schrijf het %s kunde niet! wat verveland! want: %s", user.Uid, err.Error())
-			return
+
+	if addUsers {
+		users := []protos.User{
+			{Token: "xaxaxaxa", Uid: "0", UserName: "masoud"},
+			{Token: "ihihihih", Uid: "1", UserName: "roy"},
+			{Token: "lolololo", Uid: "2", UserName: "arshia"},
 		}
-		fmt.Printf("ik schrijf het %s kunde op de database. Wat leuk! ", user.UserName)
+
+		for _, user := range users {
+			ku := fmt.Sprintf("/cluster/resources/user/%s", user.Token)
+			err = etcdMiddleware.WriteToEtcdFromPb(cli, context.Background(), ku, &user)
+			if err != nil {
+				fmt.Printf("ik kan schrijf het %s kunde niet! wat verveland! want: %s", user.Uid, err.Error())
+				return
+			}
+			fmt.Printf("ik schrijf het %s kunde op de database. Wat leuk! ", user.UserName)
+		}
 	}
 
-	protos.Role{Metadata: }
+	if addRoles {
+		roles := []protos.Role{
+			{
+				Metadata: &protos.Metadata{
+					Name:      "role1",
+					Namespace: "default",
+					Uid:       "0",
+				},
+				Rules: []*protos.PolicyRule{
+					{
+						Verbs: []string{"create"},
+						Resources: []string{
+							"pod",
+							"deployment",
+						},
+					},
+				},
+			},
+			{
+				Metadata: &protos.Metadata{
+					Name:      "role2",
+					Namespace: "default",
+					Uid:       "1",
+				},
+				Rules: []*protos.PolicyRule{
+					{
+						Verbs: []string{"create"},
+						Resources: []string{
+							"user",
+						},
+					},
+				},
+			},
+		}
+
+		for _, role := range roles {
+			ku := fmt.Sprintf("/cluster/resources/role/%s", role.Metadata.Name)
+			err = etcdMiddleware.WriteToEtcdFromPb(cli, context.Background(), ku, &role)
+			if err != nil {
+				fmt.Printf("fail. %s", err.Error())
+				return
+			}
+			fmt.Printf("soccess ")
+		}
+	}
+
+	if addRoleBinding {
+		roleBindings := []protos.RoleBinding{
+			{
+				Metadata: &protos.Metadata{
+					Name:      "roleBinding1",
+					Namespace: "default",
+					Uid:       "0",
+				},
+				RoleRef:  "role1",
+				Subjects: []string{"masoud", "arshia"},
+			},
+			{
+				Metadata: &protos.Metadata{
+					Name:      "roleBinding2",
+					Namespace: "default",
+					Uid:       "1",
+				},
+				RoleRef:  "role2",
+				Subjects: []string{"roy"},
+			},
+		}
+
+		for _, roleBinding := range roleBindings {
+			ku := fmt.Sprintf("/cluster/resources/role_binding/%s", roleBinding.Metadata.Name)
+			err = etcdMiddleware.WriteToEtcdFromPb(cli, context.Background(), ku, &roleBinding)
+			if err != nil {
+				fmt.Printf("fail. %s", err.Error())
+				return
+			}
+			fmt.Printf("soccess ")
+		}
+	}
 
 }
