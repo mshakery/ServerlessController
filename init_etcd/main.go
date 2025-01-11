18ad674afdef5a2c9a051cb51568b950258cfd72
Arshia <arshshia@gmail.com>
2025-01-11 12:36:49 +0100

    init etcd

--- init_etcd/main.go
+++ init_etcd/main.go
@@ -43,7 +43,7 @@ func main() {
 				},
 				Rules: []*protos.PolicyRule{
 					{
-						Verbs: []string{"create"},
+						Verbs: []string{"create", "retrieve", "update", "delete"},
 						Resources: []string{
 							"pod",
 							"deployment",
