7a81051a6a0a7456127211a9b804736e236e70d8
Arshia <arshshia@gmail.com>
2025-01-15 00:28:32 +0100

    dist lock update

--- etcdMiddleware/distributedlock.go
+++ etcdMiddleware/distributedlock.go
@@ -6,16 +6,15 @@ import (
 	clientv3 "go.etcd.io/etcd/client/v3"
 )
 
-func AcquireLock(cli *clientv3.Client, lockKey string, ttl int64, waitTillRelease bool) (*clientv3.LeaseGrantResponse, error) {
-	leaseResp, err := cli.Grant(context.TODO(), ttl)
+func AcquireLock(cli *clientv3.Client, lockKey string, ttl int64, waitTillAvailable bool, ctx context.Context) (*clientv3.LeaseGrantResponse, error) {
+	leaseResp, err := cli.Grant(ctx, ttl)
 	if err != nil {
 		return nil, fmt.Errorf("failed to create lease: %v", err)
 	}
 
-	txn := cli.Txn(context.TODO())
+	txn := cli.Txn(ctx)
 	txn.If(clientv3.Compare(clientv3.CreateRevision(lockKey), "=", 0)).
-		Then(clientv3.OpPut(lockKey, "locked", clientv3.WithLease(leaseResp.ID))).
-		Else()
+		Then(clientv3.OpPut(lockKey, "locked", clientv3.WithLease(leaseResp.ID)))
 
 	txnResp, err := txn.Commit()
 	if err != nil {
@@ -23,9 +22,12 @@ func AcquireLock(cli *clientv3.Client, lockKey string, ttl int64, waitTillReleas
 	}
 
 	if !txnResp.Succeeded {
-		if waitTillRelease {
-			WatchLock(cli, lockKey)
-			return AcquireLock(cli, lockKey, ttl, false)
+		if waitTillAvailable {
+			err := WatchLock(cli, lockKey, ctx)
+			if err != nil {
+				return nil, fmt.Errorf("err when watching for lock: %v", err)
+			}
+			return AcquireLock(cli, lockKey, ttl, false, ctx)
 		} else {
 			return nil, fmt.Errorf("lock exists from before")
 		}
@@ -34,22 +36,27 @@ func AcquireLock(cli *clientv3.Client, lockKey string, ttl int64, waitTillReleas
 	return leaseResp, nil
 }
 
-func ReleaseLock(client *clientv3.Client, leaseID clientv3.LeaseID) error {
-	_, err := client.Revoke(context.TODO(), leaseID)
+func ReleaseLock(client *clientv3.Client, leaseID clientv3.LeaseID, ctx context.Context) error {
+	_, err := client.Revoke(ctx, leaseID)
 	if err != nil {
 		return fmt.Errorf("failed to revoke lease: %v", err)
 	}
 	return nil
 }
 
-func WatchLock(client *clientv3.Client, lockKey string) {
-	rch := client.Watch(context.TODO(), lockKey)
+func WatchLock(client *clientv3.Client, lockKey string, ctx context.Context) error {
+	rch := client.Watch(ctx, lockKey)
 
 	for wresp := range rch {
+		if wresp.Canceled {
+			fmt.Printf("watch canceled: %v\n", wresp.Err())
+			return wresp.Err()
+		}
 		for _, ev := range wresp.Events {
 			if ev.Type == clientv3.EventTypeDelete {
-				return
+				return nil
 			}
 		}
 	}
+	return nil
 }
